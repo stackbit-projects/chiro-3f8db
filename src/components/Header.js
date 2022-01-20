260a6364d7385f041abdc306eddfad464d5153cc
Stackbit Code Editor <projects@stackbit.com>
2022-01-20 08:27:48 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -56,7 +56,7 @@ export default class Header extends React.Component {
                         >
                             <Action action={action} />
                         </li>
-                       
+                       <
                     )
                 })}
             </ul>
